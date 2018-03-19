6f5c8dfa4b83b726716fa8026103b8ab257c85d1
attacklast <fadedrecallrune@gmail.com>
2018-03-19 22:05:09 +0200

    snapshot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "animate.css/animate.min.css";
+import { render } from 'react-snapshot'
 
 import items from "./assets/data";
 import rootReducer from './reducers'
@@ -13,7 +13,7 @@ const store = createStore(rootReducer, {
   items: items
 });
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
